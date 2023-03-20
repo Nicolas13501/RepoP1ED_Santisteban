17508a97e9343fb4bf23fbc40abd7b0f1563422d
dam121 <dam121@C0103.iesataulfo.local>
2023-03-20 17:29:25 +0100

    Version numero 2 del programa

--- Proyecto_1/src/Paquete1/Principal.java
+++ Proyecto_1/src/Paquete1/Principal.java
@@ -3,7 +3,7 @@ package Paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Programa que te permitira hacer un control de versiones en Git");
+		System.out.println("Programa que te permitira hacer un control de versiones en Git. Esta sera la version 2 del programa.");
 	}
 
 }
