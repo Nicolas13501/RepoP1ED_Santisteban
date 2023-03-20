2326701a2a22423e2170ed5e87927b36f631e374
dam121 <dam121@C0103.iesataulfo.local>
2023-03-20 17:26:42 +0100

    Primera version del programa de control de versiones.

---
+++ Proyecto_1/src/Paquete1/Principal.java
@@ -0,0 +1,9 @@
+package Paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Programa que te permitira hacer un control de versiones en Git");
+	}
+
+}
